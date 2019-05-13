963328d24bf49d5abb41f058e27cd515426b7e5f
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-05-13 15:09:24 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,42 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+
+    int myFavoriteNumber = 17;
+        System.out.println(myFavoriteNumber);
+
+        String MyString = "I am a handsome moose.";
+        System.out.println(MyString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x = x + 5; Rewritten as:
+//            int x = 4;
+//            x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x; Rewritten as:
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x; Rewritten as:
+
+    byte charlie = (byte) 128;
+        System.out.println(charlie);
+
+    }
+
+}
+
