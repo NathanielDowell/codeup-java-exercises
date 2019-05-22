f165dc758b5108beb0b803901d7373c355a1cf1e
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-05-22 14:53:07 -0500

    OOP exercises completed

--- src/Person.java
+++ src/Person.java
@@ -18,26 +18,47 @@ public class Person {
         }
 
         public void sayHello() {
-            System.out.println("Hello!");
+            System.out.println("Hello, " + this.name + "!");
         }
 
+        //Constructor//
+        public Person(String name) {
+         this.name = name;
+        }
 
     public static void main(String[] args) {
 
 
-        Person nathaniel = new Person();
+        Person nathaniel = new Person("Nathaniel");
 
-        nathaniel.setName("Nathaniel");
+        nathaniel.setName("Floyd"); //resets "Nathaniel" to "Floyd"
 
         System.out.println(nathaniel.getName());
+
+        nathaniel.sayHello();
 
 
         //======= Exercise 2 =========//
 
-        Person person1 = new Person("John");
-        Person person2 = new Person("John");
-        System.out.println(person1.getName().equals(person2.getName()));
-        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName())); //true
+//        System.out.println(person1 == person2); //false
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2); //true
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());  // John
+//        System.out.println(person2.getName());  // John
+//        person2.setName("Jane");               // since person2 = person1, person1 is also set to "Jane"
+//        System.out.println(person1.getName()); // Jane
+//        System.out.println(person2.getName()); // Jane
+//        person1.setName("Bobo the Lion");      // Should set both to "Bobo the Lion"
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
 
 
     }
--- src/PersonTester.java
+++ src/PersonTester.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class PersonTester {
+
+    public static void main(String[] args) {
+        Person flamingolani = new Person("Flamingolani");
+        System.out.println(flamingolani.getName());
+
+        flamingolani.setName("Carl");
+        System.out.println(flamingolani.getName());
+
+        flamingolani.sayHello();
+    }
 }
